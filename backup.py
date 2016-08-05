58e752e90b6dc98a7e7f2bdc1cdf73db430d0d32
Andrey Petrenko <andy.petrenko@gmail.com>
2016-08-05 20:25:12 +0300

    initial backup script

--- backup.py
+++ backup.py
@@ -1,20 +1,14 @@
 # TODO: save current configs from LAB devices
-import time
 import telnetlib
 import re
 
 ipfile = open('ipaddr.txt', 'r')
 ipfile_lines = ipfile.readlines()
 
-cmd = "copy running-config http://192.168.182.42/"
-
 
 def telnet_connection(addr, port):
-    tn = telnetlib.Telnet(addr, port, timeout=2)
+    tn = telnetlib.Telnet(addr, port, timeout=3)
     tn.debuglevel(100)
-    time.sleep(1)
-    tn.read_until(b"#")
-    time.sleep(1)
 
 
 for line in ipfile_lines:
